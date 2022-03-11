896441a82e61775c60b4935592c20ea4157d5efc
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-11 15:38:15 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+const markupWithIngredients = ingredients.map(function (ingredient) {
+	const tagLi = document.createElement('li');
+
+	tagLi.textContent = ingredient;
+	tagLi.classList.add('item');
+
+	return tagLi;
+});
+
+ingredientsList.append(...markupWithIngredients);
