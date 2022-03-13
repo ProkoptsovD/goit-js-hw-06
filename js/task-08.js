45fc4e4bbb4b12530a6d19ee0f089b758774e2c0
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-13 19:03:22 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginFormRef = document.querySelector('.login-form');
 
+const handleEnteredData = (event) => {
+	event.preventDefault();
+
+	const {
+		elements: { email: email, password: password },
+	} = event.currentTarget;
+
+	!email.value || !password.value
+		? alert('Пожалуйста, заполните ВСЕ поля')
+		: console.log(`Email: ${email.value}`, `Password: ${password.value}`);
+
+	loginFormRef.reset();
+};
+
+loginFormRef.addEventListener('submit', handleEnteredData);
