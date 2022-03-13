476a4a8d07db2371e7544559518b79d4c78bcc83
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-13 19:16:48 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const changeColorBtnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+const hexColorCode = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = () => {
+	const generatedColor = getRandomHexColor();
+
+	bodyRef.style.backgroundColor = generatedColor;
+	hexColorCode.textContent = generatedColor;
+};
+
+changeColorBtnRef.addEventListener('click', changeColor);
