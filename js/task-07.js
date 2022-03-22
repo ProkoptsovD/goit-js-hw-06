a1a4204a63554e93afb2f97bbbd59f433a884734
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-22 13:32:12 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,8 @@
-const customFontSize = document.getElementById('font-size-control');
-const text = document.getElementById('text');
+const customFontSize = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
 
-const setFontSize = () => {
-	const { value: selectedFontSize } = customFontSize;
+const setFontSize = (e) => {
+	const { value: selectedFontSize } = e.currentTarget;
 
 	text.style.fontSize = `${selectedFontSize}px`;
 };
