9fb26ebce92f7acecc187b73bd7922ec5d95199b
ProkoptsovD <dimka.prokoptsov@gmail.com>
2022-03-25 15:51:18 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -88,7 +88,9 @@ function destroyBoxes() {
 
 // создает рандомный цвет
 function getRandomHexColor() {
-	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
 
 // выводит сообщение о действии пользователя
